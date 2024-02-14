b9fc0c2c6feea8533c2db690b3e779fc05074145
mingd0d <sop0327@gmail.com>
2024-02-14 15:17:06 +0900

    re-upload project file

--- shield-backend/src/main/java/shield/shieldbackend/controller/MemberController.java
+++ shield-backend/src/main/java/shield/shieldbackend/controller/MemberController.java
@@ -28,8 +28,8 @@ public class MemberController {
 
     /**
      * 아이디 중복확인
-     * 프론트에서 해당 아이디 중복 체크 버튼을 누를 때 /api/check/id?userId=userId(검사하려는 id) 형식으로 보내면 됨.
-     * 중복이 아니라면 Boolean 값인   1(true),  중복이라면   0(false)으로 결과값을 반환.
+     * 프론트에서 해당 아이디 중복 체크 버튼을 누를 때 /api/check/id?userId=userId(검사하려는 id) 형식으로 보내주시면 됩니다.
+     * 중복이 아니라면 Boolean 값인   1(true)  중복이라면   0(false)로 결과값을 반환.
      * 결과 값으로 받은 0과1로 프론트에서 중복 검사 결과 처리.
      */
     @GetMapping("/api/check/id")
@@ -40,7 +40,7 @@ public class MemberController {
 
     /**
      * 로그인
-     * MemberLoginDto로 받은 userId와 password를 통해 해당 아이디, 비밀번호가 있는지 확인 후
+     * MemberLoginDto로 받은 userId와 password를 가지고 해당 아이디, 비밀번호가 있는지 확인 후
      * 없으면 null,  있으면 해당 member의 id 반환.
      */
     @PostMapping("/api/login")
